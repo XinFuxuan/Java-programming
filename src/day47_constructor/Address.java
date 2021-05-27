b2f80113a0ba19a5bce0965539a6cb7d413f6028
XinFuxuan <x7182000@yahoo.com>
2021-05-27 19:51:52 -0400

    created getter and setter for address

---
+++ src/day47_constructor/Address.java
@@ -0,0 +1,39 @@
+package day47_constructor;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country="USA";
+
+    public void setStreet(String street) { this.street = street;}
+    public String getStreet() { return street; }
+
+    public void setCity(String city){this.city=city;}
+    public String getCity(){return city;}
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
